c69c467249e3c2bcd766db7df565de4639d047c7
Chris Riccomini <criccomi@criccomi-mn.(none)>
2014-09-25 21:18:19 -0700

    remove mac build. setup build-linux for integration to rocksdb.

--- build-linux.sh
+++ build-linux.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/env bash
 # install all required packages for rocksdb
+sudo apt-get update
 sudo apt-get -y install git make gcc g++ libgflags-dev libsnappy-dev zlib1g-dev libbz2-dev default-jdk
 
 # set java home so we can build rocksdb jars
 export JAVA_HOME=$(echo /usr/lib/jvm/java-7-openjdk*)
-ARCH=$(uname -m)
-BUILD_ROOT=/vagrant/build
-BUILD_ARCH_DIR=$BUILD_ROOT/rocksdb-linux-$ARCH
-
-# checkout rocksdb
-mkdir -p $BUILD_ROOT
-rm -rf $BUILD_ARCH_DIR
-git clone https://github.com/facebook/rocksdb.git $BUILD_ARCH_DIR
-
-# build rocksdb
-cd $BUILD_ARCH_DIR
-make -j 4 rocksdbjavastatic
+cd /rocksdb
+make clean -j 4 rocksdbjavastatic
+sudo shutdown -h now
 
